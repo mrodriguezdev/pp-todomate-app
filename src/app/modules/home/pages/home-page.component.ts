908ecac61eae7b67e1a9d80f612927ebe17a6f5d
mrodriguezdev <marioguzman_99@hotmail.com>
2024-02-05 21:26:45 -0600

    Agregando diseño inicial para los encabezados y los detalles de las tareas
    Creando formulario reactivo e imprimiendo valores en consola. Agregando imagen cuando no hayan tareas agregadas

--- src/app/modules/home/pages/home-page.component.ts
+++ src/app/modules/home/pages/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  formTask: FormGroup = new FormGroup({
+    title: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    creationDate: new FormControl('', Validators.required),
+    dateDue: new FormControl('', Validators.required),
+    priority: new FormControl('alta', Validators.required),
+  });
+
+  tareas: any = []
+
+  constructor() { }
+
+  newTask(): void {
+    console.log(this.formTask.value)
+  }
+
+  clear(): void {
+    this.formTask.reset({
+      priority: 'alta'
+    });
+  }
 }
